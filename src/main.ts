16c5b8068a4cefb99fbd56fbd78d53e9235249e2
Khyrki <khyrkimitsunobi@gmail.com>
2023-02-05 11:08:31 -0300

    feat: configura documentacao online com swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Crud Clean Architecture')
+    .setDescription(
+      'Api exemplo utilizando os conceitos de clean architecture com nestJS',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(port, () =>
+    console.log(`API disponivel em: http://localhost:${port}/swagger`),
+  );
 }
 bootstrap();
