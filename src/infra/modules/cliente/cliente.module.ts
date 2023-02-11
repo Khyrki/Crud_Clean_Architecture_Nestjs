d1562e98e4ec673ba5845c6ddb3a5f103928744a
Khyrki <khyrkimitsunobi@gmail.com>
2023-02-11 18:29:20 -0300

    chore: corrige conexao com banco

--- src/infra/modules/cliente/cliente.module.ts
+++ src/infra/modules/cliente/cliente.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClienteEntity } from '../database/mongodb/entities/cliente.entity';
 import { ClienteController } from './cliente.controller';
@@ -7,11 +6,10 @@ import {
   clienteProviders,
   clienteProvidersExports,
 } from './providers/factories';
-import { ClienteEntityRepository } from './repositories/cliente-entitu.repository';
+import { ClienteEntityRepository } from './repositories/cliente-entity.repository';
 
 @Module({
   imports: [
-    ConfigModule,
     TypeOrmModule.forFeature([ClienteEntity, ClienteEntityRepository], 'mongo'),
   ],
   controllers: [ClienteController],
--- src/infra/modules/cliente/repositories/cliente.repository.ts
+++ src/infra/modules/cliente/repositories/cliente.repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IClienteRepository } from 'src/domain/cliente/repositories/cliente-repository';
-import { ClienteEntityRepository } from './cliente-entitu.repository';
+import { ClienteEntityRepository } from './cliente-entity.repository';
 
+@Injectable()
 export class ClienteRepository implements IClienteRepository {
   constructor(
     @InjectRepository(ClienteEntityRepository, 'mongo')
